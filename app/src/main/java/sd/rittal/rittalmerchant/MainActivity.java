27fc11ecc744b8b6f509d3c320049897c8ffcee3
Mohammed Imam <32535619+mo7amd89@users.noreply.github.com>
2021-03-10 10:40:01 +0200

    add api documentations

--- app/src/main/java/sd/rittal/rittalmerchant/MainActivity.java
+++ app/src/main/java/sd/rittal/rittalmerchant/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import sd.rittal.qrsdk.uitl.RittalService;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+          RittalService.qrPurchase(getApplicationContext(),"192.168.1.1");
 
     }
 }
